8c72819ad3089c4f54bd08d5b68a7c68c439080b
CHSoufiane <soufiane.chebani@oclock.school>
2024-07-05 14:26:10 +0200

    solve server connexion

--- app/db/client-sequelize.js
+++ app/db/client-sequelize.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
 // Create a new instance of Sequelize with the connection URL
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.BASE_URL, {
   define: {
     // Using snake_case instead of default camelCase
     underscored: true,
