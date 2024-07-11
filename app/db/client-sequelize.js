da52be932bcf4e990df669a2e8c31e73c3cb6e99
CHSoufiane <soufiane.chebani@oclock.school>
2024-07-11 09:57:35 +0200

    modify sequelize client

--- app/db/client-sequelize.js
+++ app/db/client-sequelize.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.BASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
--- app/db/client.js
+++ app/db/client.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 import { Client } from 'pg';
 
-const client = new Client(process.env.BASE_URL);
+const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
 
